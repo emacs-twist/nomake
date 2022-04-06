64f10f944fe5f2433438a485bcfa5304af0e41b5
Akira Komamura <akira.komamura@gmail.com>
2022-04-07 00:56:33 +0900

    feat: Add a more comprehensive inventory configuration
    This is necessary for ELPA Core packages, GNU ELPA external single-file
    packages, etc.

--- pkgs/emacs-config/inventories.nix
+++ pkgs/emacs-config/inventories.nix
@@ -1,11 +1,18 @@
-{ gnu-elpa, melpa }:
+{ gnu-elpa, melpa, epkgs, emacs }:
 [
   {
     type = "elpa";
     path = gnu-elpa.outPath + "/elpa-packages";
+    core-src = emacs.outPath;
+    auto-sync-only = true;
   }
   {
     type = "melpa";
     path = melpa.outPath + "/recipes";
+  }
+  {
+    name = "emacsmirror";
+    type = "gitmodules";
+    path = epkgs.outPath + "/.gitmodules";
   }
 ]
